9c7d42edf4dd7a1690760605ea4c7427e5132ff1
coderGLM <779793115@qq.com>
2016-12-19 11:25:54 +0800

    修改变量名增加可读性

--- array/longest-asc-subsequence.js
+++ array/longest-asc-subsequence.js
@@ -14,27 +14,34 @@
 //     输出：3
 //
 
-// 参考：http://blog.csdn.net/chenwenshi/article/details/6027086
+//  参考：http://blog.csdn.net/chenwenshi/article/details/6027086
+//
+//  @return 最长子序列长度
+//
 function LAS(arr) {
     if (!arr || !arr.length) return 0;
 
-    let max = 0;
+    // 数组长度
     let len = arr.length;
+    // 保存到目前为止的最大长度
+    let maxLen = 0;
     // 初始子序列长度都是1
-    let lasArr = new Array(len).fill(1);
+    let MAX = new Array(len).fill(1);
     for (let i = 0; i < len - 1; i++) {
         let sub = 0;
+        // 遍历0...i-1的子序列
         for (let j = 0; j < i; j++) {
-            if (arr[j] < arr[i] && lasArr[j] > sub) {
+            if (arr[j] < arr[i] && MAX[j] > sub) {
                 sub++;
             }
         }
-        lasArr[i] = sub + 1;
-        if (lasArr[i] > max) {
-            max = lasArr[i];
-            console.log(max, i);
+        MAX[i] = sub + 1;
+        if (MAX[i] > maxLen) {
+            maxLen = MAX[i];
+            console.log(maxLen, i);
         }
     }
+    return maxLen;
 }
 
 let arr = [3, 1, 4, 8, 2, 10, 6];
